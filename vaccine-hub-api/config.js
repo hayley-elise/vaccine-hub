14dcc800cb0439b67fc031353a909e1246137314
hayley <simmons.hayley15@icloud.com>
2022-06-28 17:04:41 -0500

    Update config.js

--- vaccine-hub-api/config.js
+++ vaccine-hub-api/config.js
@@ -17,7 +17,7 @@ function getDatabaseUri() {
 }
 
 
-console.log("process.env: ".yellow, Object.keys(proccess.env))
+console.log("process.env: ".yellow, Object.keys(process.env))
 console.log("App Config".red)
 console.log("PORT: ".blue, PORT)
 console.log("Database URI: ".blue, getDatabaseUri())
