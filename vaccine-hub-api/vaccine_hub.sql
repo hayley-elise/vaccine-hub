9e518bb8180c077feaea7829c5a4edc816211520
hayley <simmons.hayley15@icloud.com>
2022-06-28 16:33:21 -0500

    Update vaccine_hub.sql

--- vaccine-hub-api/vaccine_hub.sql
+++ vaccine-hub-api/vaccine_hub.sql
@@ -2,8 +2,8 @@
 \prompt 'Press ENTER for YES or CTRL-C to CANCEL > ' answer
 
 
-DROP DATABASE vaccine_hub
-CREATE DATABASE vaccine_hub
+DROP DATABASE vaccine_hub;
+CREATE DATABASE vaccine_hub;
 \connect vaccine_hub
 
 \i vaccine_hub_schema.sql
