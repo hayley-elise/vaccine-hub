e2b91d77435bcc66278b6aecaba10d470c07b450
hayley <simmons.hayley15@icloud.com>
2022-06-28 19:42:31 -0500

    Update auth.js

--- vaccine-hub-api/routes/auth.js
+++ vaccine-hub-api/routes/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/user")
 const router = express.Router()
 
 
-router.post("/login", async(req, res, next) => {
+router.post("/login", async (req, res, next) => {
     try {
         const user = await User.login(req.body)
         return res.status(200).json({user})
@@ -12,7 +12,7 @@ router.post("/login", async(req, res, next) => {
     }
 })
 
-router.post("/register", async(req, res, next) => {
+router.post("/register", async (req, res, next) => {
     try {
         const user = await User.register(req.body)
         return res.status(201).json({user})
